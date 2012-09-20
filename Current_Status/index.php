ba2fe8fdae1b353c9dcd62941a7f44c5814a6a16
ggogun <ggogun@c8e6cdba-4040-8b7e-08a3-99ac4761913e>
2012-09-20 13:54:12 +0000

    Initialize Project using AntsLab Status Source

---
+++ Current_Status/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+        <link href="./css/reset.css" rel="stylesheet" type="text/css" media="screen"/>
+        <link href="./css/main.css" rel="stylesheet" type="text/css" media="screen"/>
+        <link href="../qunit.css" rel="stylesheet"  type="text/css" media="screen">
+        
+        <script src="./js/qunit.js"></script>
+		<script src="./js/jquery.min.js"></script>
+		<script src="./js/jquery.xdomainajax.js"></script>
+		<script src="./js/makeTable.js"></script>
+		<script src="./js/functionCollection.js"></script>
+        
+        <title> Current Status </title>
+    </head>
+    <body>
+		<div id="header">
+			
+			This is Header
+		</div>
+		
+		<div id="wrap">
+			This is wrapper
+			
+			
+			<?php
+    // this refreshes current page after 5 seconds.
+    	//header( "refresh:5;" );
+ 
+    // OR send to a new URL like this
+    	//header( "refresh:5; url=http://solidlystated.com" );
+ 
+    // OR simply echo the HTML meta tag
+    	//echo '<meta http-equiv="refresh" content="30" />';
+	?>
+	
+	
+	
+	<div id="contents">
+		<div id="headers">
+			
+
+
+			<h2 id="qunit-banner"></h2>
+		</div>
+	
+		<div id="contain" ></div>
+		
+		
+		
+
+		
+		
+		<div id = "tables">
+			
+			<center>
+				
+				
+				
+		
+				<div id="loading">
+					<img src="./images/ajax-loader.gif" /><br/>
+					<b>Loading......</b>
+				</div> 
+				
+				
+				
+				<div id="who"></div>
+			</center>
+			
+			
+
+		</div>
+		
+		
+</div>
+	
+	
+	
+	
+	
+		</div>
+		
+		<div id="footer">
+			This is footer
+			<center>
+				<img src = "./images/footer.png"><br/>
+				 CopyLeft <img src = "./images/copyleft.png" width='12px'> MJU Ants Lab.
+			</center>
+			
+			<center>
+		<div id="ads" style="width:350px;height:50px;:middle;vertical-align:middle;text-align:center;margin-top:20px;">
+			<script type="text/javascript"><!--
+google_ad_client = "ca-pub-9646834340680693";
+/* Lab_ads */
+google_ad_slot = "9630274475";
+google_ad_width = 320;
+google_ad_height = 50;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+		</div>
+		</center>
+			
+		</div>
+
+
+
+    </body>
+</html>
+
